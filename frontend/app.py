deeda6c1a0a7c77fb53520f26e9e3a5e65bb2889
ramarahul <ramarahulchintalapati@gmail.com>
2020-03-26 23:49:17 +0530

    Added Frontend with bootstrap

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello Frontend'
+    return render_template('facebook.html')
 
 
 if __name__ == "__main__":
